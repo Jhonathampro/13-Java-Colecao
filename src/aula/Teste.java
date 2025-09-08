eaf10711864457a65fca1ffe7c438bf46aa1e6a2
Jhonatham Jesus <jhonathampro@gmail.com>
2025-09-07 21:53:36 -0300

    chore: commit

--- src/aula/Teste.java
+++ src/aula/Teste.java
@@ -8,7 +8,7 @@ public class Teste {
     public static void main(String[] args) {
         List<Aluno> lista = new ArrayList<>();
         lista.add(new Aluno(10,"Davy Bernardo", "SI"));
-        lista.add(new Aluno(11,"DLucas Matheus", "Banco de dados"));
+        lista.add(new Aluno(11,"Lucas Matheus", "Banco de dados"));
         lista.add(new Aluno(12,"Juliana Nickolas", "ADM"));
 
         // Impressão da lista usando lambda
@@ -19,6 +19,14 @@ public class Teste {
        // ordenando os alunos
         lista.sort(Comparator.comparing(Aluno::getNome)
                   .thenComparing(Aluno::getCurso));
+        // sort: é o macanismo que eu faço para fazer a odenação, ele não sabe como ordenar
+        // com objetos complexos, ele apenas organiza conforme os critérios
+        // comparator: ele é a interface onde aplico os métodos de comparação
+        // comparing: ele é o método da inteface comparetor, ele serve para quando o objeto tem vários
+        // atributos e quero fazer a ordenação, ai eu passo especificamente o que ele deve ordenar.
+        // .thenComparing: ele serve para quando o primeiro comparing da empate, então faço outro critério
+        // de ordenação, e posso fazer um monte se for nécessario.
+
 
         System.out.println("======================================");
         // Impressão da lista usando lambda
--- src/aula/Teste2.java
+++ src/aula/Teste2.java
@@ -6,6 +6,9 @@ import java.util.Set;
 public class Teste2 {
     public static void main(String[] args) {
         Set<Aluno> lista = new HashSet<>();
+        // set: é uma interface que serve para não ter dados duplicados
+        // HashSet: é uma classe concreta que implemeta a ideia de não ter
+        // dados duplicados
 
         lista.add(new Aluno(10,"Davy Bernardo", "SI"));
         lista.add(new Aluno(11,"DLucas Matheus", "Banco de dados"));
