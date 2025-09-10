f710b11d373632170300f262190097812afae1a7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 09:34:38 -0300

    feat: arvore e map

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -9,17 +9,33 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Aluno> turmaBd = new HashSet<>();
-        turmaBd.add(new Aluno(10, "Matheus", "BD"));
-        turmaBd.add(new Aluno(10, "Lucas", "BD"));
-        turmaBd.add(new Aluno(15, "Selmini", "BD"));
+        turmaBd.add(new Aluno(10, "F", "BD"));
+        turmaBd.add(new Aluno(20, "Selmini", "BD"));
+        turmaBd.add(new Aluno(60, "Sel", "BD"));
 
         // LIsta de alunos matriculados em estruturas de dados
         Set<Aluno> TurmaEd = new HashSet<>();
-        turmaBd.add(new Aluno(50, "F", "F"));
-        turmaBd.add(new Aluno(20, "B", "B"));
-        turmaBd.add(new Aluno(30, "D", "D"));
+        TurmaEd.add(new Aluno(10, "F", "FI"));
+        TurmaEd.add(new Aluno(20, "B", "B"));
+        TurmaEd.add(new Aluno(30, "D", "D"));
 
-        turmaBd.forEach(Aluno -> System.out.println(Aluno));
+        // lista de todos os alunos matriculados
+        System.out.println("Alunos matriculados na duas disciplinas");
+        Set<Aluno>  uniao = new HashSet<>(turmaBd);
+        uniao.addAll(TurmaEd);
+
+        uniao.forEach(Aluno -> System.out.println(Aluno));
+
+        // Lista dos alunos matriculados nas duas disciplinas
+        System.out.println("\nAlunos matriculados na duas disciplinas");
+        Set<Aluno> nasDuas = new HashSet<>(turmaBd);
+        nasDuas.retainAll(TurmaEd);
+        nasDuas.forEach(Aluno -> System.out.println(Aluno));
+
+        System.out.println("\nAlunos matriculados apenas em banco de dados");
+        Set<Aluno> soBd = new HashSet<>(turmaBd);
+        soBd.removeAll(TurmaEd);
+        soBd.forEach(Aluno -> System.out.println(Aluno));
 
 
 
---
+++ src/ex3/Exercicio03.java
@@ -0,0 +1,28 @@
+package ex3;
+
+import java.util.*;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<Character, Integer> contador = new HashMap<>();
+        String frase;
+
+        System.out.println("Frase --> ");
+        frase = sc.nextLine();
+
+        for(char c : frase.toLowerCase().toCharArray()){
+            if(Character.isLetter(c)){
+                contador.put(c, contador.getOrDefault(c, 0) + 1);
+
+            }
+        }
+
+        // contrói uma árvore rubro-negra com as chaves
+        Set<Character> chave = contador.keySet();
+        TreeSet<Character> tree = new TreeSet<>(chave);
+        tree.forEach( c -> {
+            System.out.println(c + " --> " + contador.get(c));
+        });
+    }
+}
